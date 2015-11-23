2ca067753e7a3ef51aae630522371bf32b4080fb
adesanto <aarondesanto@gmail.com>
2015-11-23 11:49:06 -0500

    refactored a bit and moved all 3 line variables into one lines array

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 (function(){
-  var display = [
-        [" _ ", "| |", "|_|"],
-        ["   ", "  |", "  |"],
-        [" _ ", " _|", "|_ "],
-        [" _ ", " _|", " _|"],
-        ["   ", "|_|", "  |"],
-        [" _ ", "|_ ", " _|"],
-        [" _ ", "|_ ", "|_|"],
-        [" _ ", "  |", "  |"],
-        [" _ ", "|_|", "|_|"],
-        [" _ ", "|_|", " _|"]
-  ];
-  var line1 = [],
-      line2 = [],
-      line3 = [];
+
   function lcd(mainInput) {
+
+    var display = [
+          [" _ ", "| |", "|_|"], /* Zero */
+          ["   ", "  |", "  |"], /* One */
+          [" _ ", " _|", "|_ "], /* Two */
+          [" _ ", " _|", " _|"], /* Three */
+          ["   ", "|_|", "  |"], /* Four */
+          [" _ ", "|_ ", " _|"], /* Five */
+          [" _ ", "|_ ", "|_|"], /* Six */
+          [" _ ", "  |", "  |"], /* Seven */
+          [" _ ", "|_|", "|_|"], /* Eight */
+          [" _ ", "|_|", " _|"]  /* Nine */
+    ];
+    var line = [ [], [], [] ];
     var numAsArray = mainInput.toString().split('');
-    function pushToLines(i) {
-        line1.push(display[i][0]);
-        line2.push(display[i][1]);
-        line3.push(display[i][2]);
-    };
+
     numAsArray.forEach(function(numIn) {
       numIn = parseInt(numIn);
       for (var i = 0; i < 10; i++) {
@@ -29,9 +25,18 @@
         }
       };
     });
-    console.log(line1.join(' '));
-    console.log(line2.join(' '));
-    console.log(line3.join(' '));
+
+    function pushToLines(i) {
+      for (var j = 0; j < line.length; j++) {
+        line[j].push(display[i][j]);
+      }
+    };
+
+    console.log(line[0].join(' '));
+    console.log(line[1].join(' '));
+    console.log(line[2].join(' '));
+
   };
-  lcd(888);
+
+  lcd(001);
 })();
